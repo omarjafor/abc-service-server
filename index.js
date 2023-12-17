1bc57f75d5a6a8a8c43808a0c34ad94d1420d1a1
omarjafor <omarjaforchy@gmail.com>
2023-12-17 15:21:05 +0600

    some update and issue fixed

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-}));
+
 
 app.use(express.json());
 app.use(cookieParser());
