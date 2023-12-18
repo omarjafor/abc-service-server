19ce2893d66eeb51631756c0ac2d064606479a34
omarjafor <omarjaforchy@gmail.com>
2023-12-18 21:28:21 +0600

    some update and issue fixed

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express();
+
 const port = process.env.PORT || 5000;
 
 app.use(cors({
