e94bdef8614b20525d4ec480fb3265520bd31ca3
omarjafor <omarjaforchy@gmail.com>
2023-12-18 21:28:03 +0600

    some update and issue fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 app.use(cors({
     origin: ['http://localhost:5173'],
